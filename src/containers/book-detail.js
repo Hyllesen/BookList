a98dd98d8b59b2a3715d900bc3c7d76258549d26
Development <Development@Apps-Mac-mini.local>
2017-04-03 16:48:45 +0200

    First commit

---
+++ src/containers/book-detail.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default BookDetail extends Component {
+    render() {
+        return (
+            <div>Book detail!</div>                
+        );
+    }
+}
